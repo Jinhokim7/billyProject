73d115e1ce4c7d6b9e6543ab005471220059557d
sangyeon4 <109186387+sangyeon4@users.noreply.github.com>
2022-12-06 17:05:01 +0900

    Update billyController.java

--- BillyProject/src/main/java/com/billy/main/billyController.java
+++ BillyProject/src/main/java/com/billy/main/billyController.java
@@ -18,7 +18,7 @@ import com.billy.VO.BillyGoodsVO;
 @Controller
 public class billyController {
 	
-	@Inject		// �굹 �꽌鍮꾩뒪 二쇱엯
+
 	private IF_billyService bsrv;
 	
 	@RequestMapping(value = "/joinForm", method = RequestMethod.GET)
@@ -29,8 +29,6 @@ public class billyController {
 	
 	@RequestMapping(value = "/joinAction", method = RequestMethod.POST)
 	public String joinAction(Locale locale, Model model, BillyMemberVO bmVO) throws Exception { 
-		System.out.println(bmVO.getId()+"---�뵒踰꾧퉭");
-		System.out.println(bmVO.getName()+"--�씠由�");
 		return "redirect:/home";
 	}
 	
@@ -47,8 +45,6 @@ public class billyController {
 	}
 	@RequestMapping(value = "/billeyAction", method = RequestMethod.POST)
 	public String billeyAction(Locale locale, Model model, BillyGoodsVO bvo, String gName ) throws Exception{
-		//媛앹껜濡� 諛쏆쓣 �븣�뒗 �뙆�씪誘명꽣 �씠由꾧낵 媛앹껜�쓽 蹂��닔�쓽 �씠由꾩씠 �씪移섑븯怨�  getter,setter媛� �엳�뼱�빞�븳�떎.>>�옄�룞留ㅽ븨
-		System.out.println(bvo.getgEndDate()+"---�뵒踰꾧퉭�슜�룄"); 
 		System.out.println(gName);
 		System.out.println(bvo.getCateNum());
 		System.out.println(bvo.getgPrice());
